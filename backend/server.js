5e0d1e9fdd70549f1ef4a36e8807823ec5c0b15d
bharatvarshan <bharatvarshan.cse@gmail.com>
2022-05-25 01:15:15 +0530

    [MODIFY] Added CORS

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ require("./auth/auth");
 let app = express();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://proacademyfrontend.s3-website.ap-south-1.amazonaws.com/"
+  );
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE"
